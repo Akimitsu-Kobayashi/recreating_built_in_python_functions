9c659a7c7811862b6788c5618ca28f48102b0bec
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-27 18:50:40 +0800

    unsure of what to make of the program for it is asking to recreate the funtion but it wont be user friendly if I just make the funtion itself so I also made it so that the program automatically just asks the user anyways

--- remove_prefix.py
+++ remove_prefix.py
@@ -3,10 +3,20 @@
 
 
 #ask user for text
+text = input("Enter Text: ")
+
 #ask for the prefix to be removed
+prefix = input("Enter prefix to be removed: ")
 
 #create a defined function
+def remove_prefix(text, prefix):
     #in any begining of the text if it is equal to the parameter to be removed it will be removed
-    #else just print the text
+    if text[0:len(prefix)] == prefix:
+        return text[len(prefix):]
+    else:
+        return text
+
+result = remove_prefix(text,prefix)
 
 #print the resulting text
+print(result)
