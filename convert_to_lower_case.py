2663be952a84ac2d9e645c81bde2b7b78b9444e8
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-27 19:50:44 +0800

    iterated to each one of the list of alphabet in lower and in uppercase, there is a btter solution for this but this is a way with using the least amount of built in functions

---
+++ convert_to_lower_case.py
@@ -0,0 +1,21 @@
+#Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+
+
+lowercase_letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+uppercase_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+#input text
+text = input("Enter text: ")
+result = ""
+
+#if the letter in text is upper case turn to lower case
+for letters in text:
+    if letters in uppercase_letters:
+        for alphabet in range(len(uppercase_letters)):
+            if uppercase_letters[alphabet] == letters:
+                result += lowercase_letters[alphabet]
+    else:
+        result += letters
+
+#print text
+print(result)
