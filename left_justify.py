b0be185e7d09d7d765e290ec7aaa915b80182b4a
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-27 20:46:43 +0800

    left adjusts working unexpectedly where multiplying a string by a negative number resulted in an empty string

---
+++ left_justify.py
@@ -0,0 +1,17 @@
+#Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+
+#ask user for text
+text = input("Enter Text: ")
+
+#ask what the text should be spaced with
+spaces_with = input("space with: ")
+
+#ask the count of the left adjust
+spaces_count = int(input("How many: "))
+
+result = text + (spaces_with*(spaces_count - len(text)))
+
+print(result + "|")
+
+
+
