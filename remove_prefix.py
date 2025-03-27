2a6b545b7fda402058c96d718efdecca35f965b1
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-27 18:35:35 +0800

    psuedocode for remove prefix

---
+++ remove_prefix.py
@@ -0,0 +1,12 @@
+#Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
+
+
+
+#ask user for text
+#ask for the prefix to be removed
+
+#create a defined function
+    #in any begining of the text if it is equal to the parameter to be removed it will be removed
+    #else just print the text
+
+#print the resulting text
