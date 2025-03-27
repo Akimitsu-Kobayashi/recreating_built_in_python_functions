e0c384f231902f8c332513b58a7dc18943d128f1
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-27 20:28:34 +0800

    checks if text endswith suffix

---
+++ check_if_text_endswith_another_text.py
@@ -0,0 +1,13 @@
+#Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+
+#ask user for text
+text = input("Enter Text: ")
+
+#ask for what to check the texts ends with to be removed
+endswith = input("does the word ends with: ")
+
+
+if text[len(text)-len(endswith):] == endswith:
+    print("True")
+else:
+    print("False")
