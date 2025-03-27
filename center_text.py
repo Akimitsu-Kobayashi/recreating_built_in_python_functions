8741b6118454b89640763ecb4a991d47d550f788
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-27 22:20:02 +0800

    centers text

---
+++ center_text.py
@@ -0,0 +1,19 @@
+#Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+
+#ask user for text
+text = input("Enter Text: ")
+
+#ask what the text should be spaced with
+spaces_with = input("space with: ")
+
+#ask the count yo br centered
+space_count = int(input("How many: "))
+
+all_spaces = space_count - len(text)
+
+left_spacing = all_spaces//2
+right_spacing = all_spaces - left_spacing
+
+result = spaces_with*left_spacing + text + spaces_with*right_spacing
+
+print(result)
